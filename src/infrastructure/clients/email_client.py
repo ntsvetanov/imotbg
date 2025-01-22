f1c667b0f449f6ff563cb4f6fdcb5fc78e8d70ea
Nikolay Nikolaev <nikolay.nikolaev@living.homes>
2025-01-22 09:18:27 +0000

    latest

--- src/infrastructure/clients/email_client.py
+++ src/infrastructure/clients/email_client.py
@@ -12,6 +12,9 @@ class EmailClient:
         self.send_to_email = app_config.mailtrap_send_to_email
         self.token = app_config.mailtrap_token
         self._client = mt.MailtrapClient(token=self.token)
+        print(f"Email client initialized with sender email: {self.sender_email}")
+        print(f"Email client initialized with send to email: {self.send_to_email}")
+        print("host = ", app_config.mailtrap_host)
 
     def send_email(
         self,
